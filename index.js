faf451d75d1e16b1a65ce7eb168a770527f4fe7b
ruvindya28 <ruvindyasachini2000@gmail.com>
2025-04-06 00:29:22 +0530

    .env implimentation

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ import userRouter from './routes/userRouter.js';
 import productRouter from './routes/productRouter.js';
 import verifyJWT from './middleware/auth.js';
 import orderRouter from './routes/orderRouter.js';
+import dotenv from 'dotenv';
+dotenv.config();
 
 
 
 const app = express();
 
-mongoose.connect("mongodb+srv://admin:200028@cluster0.0fdvs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(
+mongoose.connect(process.env.MONGO_URL).then(
     ()=>{
         console.log("Connected to the database");
     }
