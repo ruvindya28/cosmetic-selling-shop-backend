93b7ef75e65422188770966ea25adca6fbbf541b
ruvindya28 <ruvindyasachini2000@gmail.com>
2025-04-06 00:34:46 +0530

    jwt key hidden

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,11 +1,13 @@
 
 import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv .config();
 export default function verifyJWT(req,res,next){
     const header=req.headers["authorization"];
     if(header!=null ){
       const token=header.replace("Bearer ","");
       console.log(token);
-      jwt.verify(token,"random456",(err,decoded)=>{
+      jwt.verify(token,process.env.JWT_KEY,(err,decoded)=>{
           console.log(decoded);
           if(decoded != null){
             req.user=decoded;
