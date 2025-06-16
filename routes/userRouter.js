ff2f276c2da6dc72783f7cd6d2ce665a19883295
ruvindya28 <ruvindyasachini2000@gmail.com>
2025-06-17 00:03:26 +0530

    add sendOtp route to userRouter

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { deleteUser, getAllUsers, getCurreentUser, googleLogin, loginUser, saveUser } from '../controllers/userController.js';
+import { deleteUser, getAllUsers, getCurreentUser, googleLogin, loginUser, saveUser, sendOtp } from '../controllers/userController.js';
 
 
 const userRouter = express.Router();
@@ -8,6 +8,7 @@ userRouter.post("/",saveUser)
 userRouter.post("/login",loginUser)
 userRouter.post("/google", googleLogin);
 userRouter.get("/current",getCurreentUser);
+userRouter.post("/sendMail",sendOtp);
 
 userRouter.get("/admin/all", getAllUsers);
 userRouter.delete("/:userId",deleteUser)
